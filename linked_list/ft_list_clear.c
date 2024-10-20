ebfa489222426f4b6414670ae077e92d1ee48467
Zakaria Adriouche <zakriaadriouche@gmail.com>
2024-10-20 18:47:43 +0100

    change

--- linked_list/ft_list_clear.c
+++ linked_list/ft_list_clear.c
@@ -1,33 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list_clear.c                                    :+:      :+:    :+:   */
+/*   ft_list_clear_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zadriouc@student.1337.ma <zadriouc>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/22 11:17:54 by zadriouc@st       #+#    #+#             */
-/*   Updated: 2024/09/25 15:38:06 by zadriouc@st      ###   ########.fr       */
+/*   Created: 2023/11/25 12:13:41 by zadriouc          #+#    #+#             */
+/*   Updated: 2024/09/25 15:07:41 by zadriouc@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_a_list.h"
 
-void ft_list_clear(t_env **begin_list, void (*free_fct)(void *))
+void	ft_list_clear_bonus(t_env **lst, void (*del)(void *))
 {
-    t_env   *delter;
-    int     size;
+	t_env	*tmp;
 
-	if (!begin_list || !(*begin_list) || !free_fct)
+	if (!lst || !(*lst) || !del)
 		return ;
-    delter = *begin_list;    
-    size = 0;
-    // while (delter)
-    // {
-    //     delter = delter->next;
-    //     size++;
-    // }
-    size = ft_list_size(*begin_list);
-    printf("size is %d\n", size);
-    while (size--)
-        ft_list_del_last(begin_list, free_fct);
+	while ((*lst)->next != NULL)
+	{
+		tmp = (*lst)->next;
+		ft_list_del_first(lst, del);
+		*lst = tmp;
+	}
+	ft_list_del_first(lst, del);
+	*lst = NULL;
 }
