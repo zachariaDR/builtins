5c4332be99c83126cc59025e3ef0f6edbcdc8a56
Zakaria Adriouche <zakriaadriouche@gmail.com>
2024-10-20 18:40:20 +0100

    change1

--- builtins.c
+++ builtins.c
@@ -6,10 +6,6 @@ int main(int ac, char **av, char **env)
 {
     ft_init_data(ac, av, env);
     // ft_print_list(&g_env);
-    ft_env(NULL);
-
-    ft_unset("SHLVL");
-    ft_env(NULL);
 
     return (0);
 }
--- ft_init_data.c
+++ ft_init_data.c
@@ -4,7 +4,7 @@ void    ft_init_data(int ac, char **av, char **env)
 {
     (void)ac;
     (void)av;
-    // ft_init_bi(); i will use it to init every function for builtins
+
     g_env = ft_fill_env(env);
     
 }
