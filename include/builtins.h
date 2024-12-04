eed96d7559c7d5b8d5f1f4572686ece854540de3
Zakaria Adriouche <zakriaadriouche@gmail.com>
2024-12-04 02:03:20 +0100

    change builtins prototypes

--- include/builtins.h
+++ include/builtins.h
@@ -6,7 +6,7 @@
 /*   By: zadriouc@student.1337.ma <zadriouc>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 01:47:29 by msennane          #+#    #+#             */
-/*   Updated: 2024/11/28 15:15:11 by zadriouc@st      ###   ########.fr       */
+/*   Updated: 2024/12/04 00:04:43 by zadriouc@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@
 void    ft_env(t_env_var *env_vars);
 void	ft_export(t_env_var **env_vars, char **args);
 void	ft_unset(t_env_var **env_vars, char **args);
-void	ft_pwd(char **args);
+void	ft_pwd(int *exit_status, char *argv);
 void	ft_cd(t_env_var **env_vars, char **args);
-void    ft_echo(int ac, char **av);
-void 	ft_exit(int ac, char **av);
+void    ft_echo(char **av);
+void 	ft_exit(char **av, int *exit_status);
 
 
 #endif
